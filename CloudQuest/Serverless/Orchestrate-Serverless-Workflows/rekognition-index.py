1d45a7967cc02541524c774b5bbb54463eb6f587
Calvin Chen <tkchen7@govcomm.com>
2024-03-24 07:54:10 -0400

    Serverless Orchestrate step functions - SNS Fanout

---
+++ CloudQuest/Serverless/Orchestrate-Serverless-Workflows/rekognition-index.py
@@ -0,0 +1,17 @@
+
+# Boto3 - Rekognition Client
+# More Info: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rekognition.html
+rekognition_client = boto3.client('rekognition')
+
+
+def lambda_handler(event, context):
+    logger.info(event)
+    response = rekognition_client.detect_moderation_labels(
+        Image={"S3Object": {"Bucket": event['bucket'], "Name": event['key']}})
+    logger.info(response)
+    moderation_labels = response['ModerationLabels'] if 'ModerationLabels' in response else None
+    if not moderation_labels:
+        return {'safe_content': True}
+    else:
+        return {'safe_content': False}
+
