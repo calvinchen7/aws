24042f55f07318742bb2cdd9b1fc2b831227dc9e
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-09 13:13:57 -0400

    StringBuilder String toString() comparision

---
+++ java/JavaToString/Main.java
@@ -0,0 +1,17 @@
+import java.lang.StringBuilder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("Java");
+		String s = "Java";
+
+		if (sb.toString().equals(s.toString())) {
+			System.out.println("Match 1");
+		} else if(sb.equals(s)) {
+			System.out.println("Match 2");
+		} else {
+			System.out.println("No Match");
+		}
+	}
+}
