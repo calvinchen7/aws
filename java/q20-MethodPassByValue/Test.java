b0ea87a64925c555b825168a2efba1d29f655352
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-20 11:16:47 -0400

    Unhandled Exception
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q19-UnhandledException> javac Test.java
    Test.java:14: error: unreported exception Exception; must be caught or declared to be
    thrown
            ex.readCard(cardNo);
                       ^
    1 error

---
+++ java/q20-MethodPassByValue/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+        public static void main(String[] args) {
+        int x = 5;
+            while(isAvailable(x)) {
+            System.out.print(x);
+            x--; // Answer B - Its ok.
+            }
+        }
+        
+        public static boolean isAvailable (int x) {
+        return x-- > 0 ? true : false;
+        }
+    }
