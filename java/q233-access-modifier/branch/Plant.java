75352024d7fda500cb507e83da8a8ef5d3c2b4a9
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-20 19:16:39 -0400

    Access modifier
    java root\Tree.java
    branch\Plant.java:13: error: m2() has private access in Tree
            t.m2();
             ^
    branch\Plant.java:14: error: m3() has protected access in Tree
            t.m3();
             ^
    branch\Plant.java:15: error: m4() is not public in Tree; cannot be accessed from outside package
            t.m4();

---
+++ java/q233-access-modifier/branch/Plant.java
@@ -0,0 +1,17 @@
+package branch;
+import root.Tree;
+
+public class Plant extends Tree{
+    public void m1(){}
+    public void m2(){}
+    public void m3(){}
+    public void m4(){}
+
+    public static void main(String[] args){
+        Tree t = new Plant();
+        t.m1();
+        t.m2();
+        t.m3();
+        t.m4();
+    }
+}
