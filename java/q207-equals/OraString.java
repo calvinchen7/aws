ebdfe0b79154c9a3b27ac8d444fa82110b19a158
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-20 13:04:52 -0400

    String and Object comparison equals and equalsIgnoreCase
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q207-equals> java Test
    B
    C

---
+++ java/q207-equals/OraString.java
@@ -0,0 +1,11 @@
+class OraString {
+    
+    String s;
+    public boolean equals (OraString str) {
+        return this.s.equalsIgnoreCase(str.toString());
+    }
+
+    OraString(String s){
+    this.s = s;
+    }
+}
---
+++ java/q207-equals/Test.java
@@ -0,0 +1,22 @@
+
+
+
+    
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "Moon";
+        OraString s2 = new OraString("Moon");
+        
+        if((s1 == "Moon") && (s2.equals("Moon"))) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
+
+        if (s1.equalsIgnoreCase(s2.s)) {
+            System.out.println("C");
+        } else {
+            System.out.println("D");
+        }
+    }
+}
