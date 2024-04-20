31eb8fea0b4fde8ccfd29001f52c76597adf7556
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-20 11:26:48 -0400

    Static Method and field
    App.java:6: error: non-static variable foo cannot be referenced from a static context
            foo += 10;
            ^
    1 error

---
+++ java/q208-StaticMethodAndField/App.java
@@ -0,0 +1,16 @@
+public class App {
+    int foo;
+    static int bar;
+
+    static void process() {
+        foo += 10;
+        bar += 10;
+    }
+
+    // Entry point of the program
+    public static void main(String[] args) {
+        App app = new App();
+        app.process();
+        System.out.println(app.foo);
+    }
+}
