505f31d9691e116aabfe2718fe67cf78f7fa4738
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-20 16:44:33 -0400

    Update FieldInit.java
    FieldInit.java:12: error: incompatible types: possible lossy conversion from double to float
            double y1 = 203.22; float fit = y1;
                                            ^
    FieldInit.java:14: error: incompatible types: double cannot be converted to Float
            Float fit2 = 100.00;
                         ^
    2 errors

--- java/q220-primitive-object-initial/FieldInit.java
+++ java/q220-primitive-object-initial/FieldInit.java
@@ -8,5 +8,20 @@ public class FieldInit {
         System.out.println(f.c);
         System.out.println(f.b);
         System.out.println(f.f);
+
+        double y1 = 203.22; float fit = y1;
+        float fit1 = (float) 1_11.00;
+        Float fit2 = 100.00;
+        int y2 = 100; float fit3 = (float) y2;
+        float fit4 = 100.00F;
+
+        System.out.println(fit);
+        System.out.println(fit1);
+        System.out.println(fit2);
+        System.out.println(fit3);
+        System.out.println(fit4);
+
+
+
     }   // Output: null false 0.0 
 }
