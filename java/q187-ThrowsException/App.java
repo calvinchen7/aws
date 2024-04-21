08b2fadd50065244c1c8609b8649dc029f0a55c0
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-21 18:03:33 -0400

    Exception handling
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q187-ThrowsException> java App
    Opened
    Completed

---
+++ java/q187-ThrowsException/App.java
@@ -0,0 +1,30 @@
+import java.lang.Exception;
+import java.lang.RuntimeException;
+
+class LogFileException extends Exception {}
+class AccessViolationException extends RuntimeException {}
+
+
+class App {
+    public static void main(String[] args) throws LogFileException {
+        App obj = new App();
+        try {
+            obj.open();
+            obj.process();
+            throw new LogFileException();
+        }
+        catch (Exception e) {
+            System.out.println("Completed");
+        }
+    }
+
+    void process() throws LogFileException{
+        System.out.println("Processed");
+        throw new LogFileException();
+    }
+
+    void open() {
+        System.out.println("Opened");
+        throw new AccessViolationException();
+    }
+}
