28e197bc5e99fe86556f3f77018e3cf80ec078b1
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-21 17:46:54 -0400

    Integer
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q186-integer> java Test
    7 : 2 : 3

---
+++ java/q186-integer/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = 1;
+        int r1 = a * b / c + 1;
+        int r2 = a / b * c + 1;
+        int r3 = a * (b / (c + 1));
+        System.out.println(r1 + " : " + r2 + " : " + r3);
+    }
+}
