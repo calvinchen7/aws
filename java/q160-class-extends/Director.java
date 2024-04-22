b55a53a480951f69c5439ebf4e76ae973bca0f81
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-22 18:04:18 -0400

    Class Extends

--- java/q160-class-extends/Director.java
+++ java/q160-class-extends/Director.java
@@ -1,14 +1,23 @@
-    class Employee{
+    
+    class Employee implements Badge{
         public int salary;
+
+        public void displayBadge() {};
     }
     
     class Manager extends Employee{
         public int budget;
+
+        public void displayBadge() {};
     }
     
     public class Director extends Manager{
     
         public int stockOptions;
+        
+        public void getStockOptions() {};
+
+        public void displayBadge() { System.out.print("Director Badge"); };
     
         public static void main(String[] args) {
 
@@ -22,7 +31,14 @@
             // manager.stockOption = 500; // Compilation Error
             // employee.budget = 200_000; // Compilation Error
             director.salary = 80_000;
-        
+
+
+            Director dir = new Director();
+            dir.getStockOptions();
+            dir.displayBadge();
+
+
+            
         }
     
     }
