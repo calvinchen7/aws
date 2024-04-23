79e271d6f8f3890c600cc9476a4748fb517d5af3
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-23 19:33:35 -0400

    break continue
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q151-break-continue> java Test
    A B C D Work done

---
+++ java/q151-break-continue/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+
+    public static void main(String[] args) {
+        String[] arr = { "A", "B", "C", "D" };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if(arr[i].equals("D")) {
+            System.out.println("Work done");
+                break;
+            }
+            continue;
+        }
+    }
+}
