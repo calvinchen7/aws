ab82d142f4fbcadf6d90872fec840cccacbdd360
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-23 21:23:44 -0400

    extends constructor
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q147-extends-constructor> java Test
    20:20

---
+++ java/q147-extends-constructor/Test.java
@@ -0,0 +1,34 @@
+    class Vehicle {
+        int x;
+        Vehicle() {
+            this(10); // line n1
+        }
+
+        Vehicle(int x) {
+            this.x = x;
+        }
+    }
+    
+    class Car extends Vehicle {
+        int y;
+        Car() {
+        super(10); // line n2
+        }
+        Car(int y) {
+        super(y);
+        this.y = y;
+        }
+    
+        public String toString() {
+            return super.x + ":" + this.y;
+        }
+    }
+    
+    public class Test {
+    
+        public static void main(String[] args) {
+            Vehicle y = new Car(20);
+            System.out.println(y);
+        }
+    
+    }
