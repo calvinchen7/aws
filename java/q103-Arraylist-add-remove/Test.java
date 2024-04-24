54ef64ce71f9bb88e496f8b9007dea06a5dec771
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-24 15:22:44 -0400

    ArrayList add remove
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q103-Arraylist-add-remove> java Test
    [1, 3, 4]

---
+++ java/q103-Arraylist-add-remove/Test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> points = new ArrayList<>();
+        points.add(1);
+        points.add(2);
+        points.add(3);
+        points.add(4);
+        points.add(null);
+        points.remove(1);
+        points.remove(null);
+        System.out.println(points);
+    }
+}
