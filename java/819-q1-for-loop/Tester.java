59fdcf4f66b13db121fd13f815b77a24afb01dc5
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-24 17:54:23 -0400

    for loop mode continue
    PS C:\Users\Calvin\Documents\GitHub\aws\java\819-q1-for-loop> java Tester
    1-5

---
+++ java/819-q1-for-loop/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    public static void main(String[] args) {
+    int x = 0, y = 6;
+        for (; x < y; x++, y--) {
+            if (x % 2 == 0) {
+                continue;
+            }
+            System.out.println(x + "-" + y);
+        }
+    }
+    
+}
