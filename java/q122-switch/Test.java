cb5c669095e77f56c7266614f48b497eb01d62bd
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-24 08:34:30 -0400

    switch
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q122-switch> java Test
    -1 sun
    0 mon
    2 wed
    4 wed
    3 sun
    3

---
+++ java/q122-switch/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+    public static void main (String [] args) {
+
+        int wd = 0;
+        String days[] = {"sun", "mon", "wed", "sat"};
+        for (String s : days) {
+            switch (s) {
+                case "sat":
+                case "sun":
+                wd--;
+                System.out.println(wd + " sun ");
+                break;
+                case "mon":
+                wd++;
+                System.out.println(wd + " mon ");
+                case "wed":
+                wd += 2;
+                System.out.println(wd + " wed ");
+            }
+        }
+        System.out.print(wd + " ");
+    }
+}
