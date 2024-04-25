17fcf251f6ebf7a45bfad0584fe006b20bd5d49a
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-25 07:44:39 -0400

    unary
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q83-unary> javac *.java
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q83-unary> java Test
    10 Hello Universe!

---
+++ java/q83-unary/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int aVar = 9;
+        
+        if(aVar++ < 10) {
+        System.out.println(aVar + " Hello Universe!");
+        } else {
+        System.out.println(aVar + " Hello World!");
+        }
+        
+    }
+
+}
