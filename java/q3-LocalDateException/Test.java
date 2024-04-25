cf607a39f277652cd81fcf32c0a86666505c50f8
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-25 17:00:34 -0400

    UnsupportedTemporalTypeException
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q3-LocalDateException> javac *.java
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q3-LocalDateException> java Test
    Exception in thread "main" java.time.temporal.UnsupportedTemporalTypeException: Unsupported field: HourOfDay
            at java.base/java.time.LocalDate.get0(LocalDate.java:698)
            at java.base/java.time.LocalDate.getLong(LocalDate.java:678)
            at java.base/java.time.format.DateTimePrintContext.getValue(DateTimePrintContext.java:308)
            at java.base/java.time.format.DateTimeFormatterBuilder$NumberPrinterParser.format(DateTimeFormatterBuilder.java:2904)
            at java.base/java.time.format.DateTimeFormatterBuilder$CompositePrinterParser.format(DateTimeFormatterBuilder.java:2525)
            at java.base/java.time.format.DateTimeFormatterBuilder$CompositePrinterParser.format(DateTimeFormatterBuilder.java:2525)
            at java.base/java.time.format.DateTimeFormatterBuilder$CompositePrinterParser.format(DateTimeFormatterBuilder.java:2525)
            at java.base/java.time.format.DateTimeFormatter.formatTo(DateTimeFormatter.java:1903)
            at java.base/java.time.format.DateTimeFormatter.format(DateTimeFormatter.java:1877)
            at java.base/java.time.LocalDate.format(LocalDate.java:1797)
            at Test.main(Test.java:7)

--- java/q3-LocalDateException/Test.java
+++ java/q3-LocalDateException/Test.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class Test{
    public static void main(String[] args) {
-	String date = LocalDate.parse("2014-05=04").format(DateTimeFormatter.ISO_DATE_TIME);
+	String date = LocalDate.parse("2014-05-04").format(DateTimeFormatter.ISO_DATE_TIME);
 	System.out.println(date);
   }
 }
