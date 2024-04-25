49a57510f3adaeda17b9430e8a73f22278c995ac
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-25 12:56:10 -0400

    access modifier
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q43-access-modifier> javac *.java
    Test.java:12: error: p is not public in Acc; cannot be accessed from outside package
            System.out.println(obj.p);
                                  ^
    Test.java:13: error: q has private access in Acc
            System.out.println(obj.q);
                                  ^
    Test.java:14: error: r has protected access in Acc
            System.out.println(obj.r);
                                  ^
    3 errors

---
+++ java/q43-access-modifier/Acc.java
@@ -0,0 +1,10 @@
+// Acc.java
+
+package p1;
+
+public class Acc {
+int p = 0;
+private int q = 1;
+protected int r = 2;
+public int s = 3;
+}
---
+++ java/q43-access-modifier/Test.java
@@ -0,0 +1,18 @@
+// Test.java
+
+package p2;
+import p1.Acc;
+
+public class Test extends Acc {
+
+    public static void main(String[] args) {
+
+        Acc obj = new Test();
+
+        System.out.println(obj.p);
+        System.out.println(obj.q);
+        System.out.println(obj.r);
+        System.out.println(obj.s);
+
+    }
+}
