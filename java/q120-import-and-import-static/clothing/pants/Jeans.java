6ff89f00a731279e6dd3602858cc1d7eca792ac1
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 19:22:25 -0400

    import static import
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q120-import-and-import-static> javac .\clothing\Shirt.java .\clothing\pants\Jeans.java

---
+++ java/q120-import-and-import-static/clothing/pants/Jeans.java
@@ -0,0 +1,17 @@
+package clothing.pants;
+
+import clothing.Shirt;
+
+public class Jeans {
+    public void matchShirt () {
+        String color = Shirt.getColor();
+        if(color.equals("Green")) {
+        System.out.print("Fit");
+        }
+    }
+    public static void main(String[] args) {
+    Jeans trouser = new Jeans();
+    trouser.matchShirt();
+    }
+
+}
