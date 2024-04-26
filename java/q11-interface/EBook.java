86e0072e6257256ee9062412534769b84a00f806
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-25 21:01:54 -0400

    abstract method
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q11-interface> javac *.java
    EBook.java:6: error: EBook is not abstract and does not override abstract method setBookMark() in Readable
    public class EBook extends Book {
           ^
    1 error

---
+++ java/q11-interface/EBook.java
@@ -0,0 +1,12 @@
+
+abstract class Book implements Readable {
+    public void readBook() {};
+}
+
+public class EBook extends Book {
+    public void readBook() {};
+
+    public static void main(String[] args) {
+        
+    }
+}
