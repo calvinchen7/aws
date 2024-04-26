0f0598a451b82aa7bf9bb5428902ad46f233da11
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 16:23:05 -0400

    ClassCastException

--- java/q95-class-casting/C.java
+++ java/q95-class-casting/C.java
@@ -17,13 +17,32 @@ public class C extends A {
     }
     
     public static void main(String[] args) {
-    A b1 = new A ();
-    A b2 = new C ();
-    
-    b1 = (A) b2; //line n1
-    A b3 = (B) b2; //line n2
-    
-    b1.test ();
-    b3.test ();
+        A b1 = new A ();
+        A b2 = new C ();
+        A b4 = new B ();
+
+        b1.test();
+        b2.test();
+        b4.test();
+        
+        b1 = (A) b2; //line n1
+        b1.test ();
+        b2.test ();
+
+        A b3 = (B) b2; //line n2
+        b3.test ();
+
+/* 
+PS C:\Users\Calvin\Documents\GitHub\aws\java\q95-class-casting> javac *.java
+PS C:\Users\Calvin\Documents\GitHub\aws\java\q95-class-casting> java C
+A
+C
+B
+C
+C
+Exception in thread "main" java.lang.ClassCastException: class C cannot be cast to class B (C and B are in unnamed module of loader 'app')
+        at C.main(C.java:32)
+*/
+
     }
 }
