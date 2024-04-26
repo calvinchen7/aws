bbb7780a682024ce78185e837b77eb62443ff550
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 07:55:48 -0400

    field initial
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q17-field-initial> java CheckingAccount
    0
    0
    0

--- java/q11-interface/EBook.java
+++ java/q11-interface/EBook.java
@@ -5,6 +5,7 @@ abstract class Book implements Readable {
 
 public class EBook extends Book {
     public void readBook() {};
+    public void setBookMark() {};
 
     public static void main(String[] args) {
         
