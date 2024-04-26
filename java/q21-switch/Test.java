e36787c400ad8e501e1d8f7baf9578aea682e4da
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 09:34:43 -0400

    error: incompatible types: int cannot be converted to String
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q24-compilation-error-string-integer> javac *.java
    Test.java:6: error: incompatible types: int cannot be converted to String
            String s3 = (String) (s1 + s2); // line n1
                                 ^
    1 error

--- java/q21-switch/Test.java
+++ java/q21-switch/Test.java
@@ -24,3 +24,11 @@ public class Test {
     
     
     }
+
+/* 
+PS C:\Users\Calvin\Documents\GitHub\aws\java\q24-compilation-error-string-integer> javac *.java
+Test.java:6: error: incompatible types: int cannot be converted to String
+        String s3 = (String) (s1 + s2); // line n1
+                             ^
+1 error
+*/
