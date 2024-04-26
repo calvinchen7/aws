2b7f5ca13ed03fb93883e63e5e2043d217d82c47
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 18:48:50 -0400

    throwIllegalAccessError
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q116-interface-public> javac *.java
    Test.java:7: error: export() in Tool cannot implement export() in Exportable
            protected void export () { // line n1 - throwIllegalAccessError
                           ^
      attempting to assign weaker access privileges; was public
    1 error

---
+++ java/q116-interface-public/Test.java
@@ -0,0 +1,29 @@
+
+    interface Exportable {
+        void export();
+    }
+        
+    class Tool implements Exportable {
+        protected void export () { // line n1 - throwIllegalAccessError
+            System.out.println("Tool::export");
+        }
+    }
+        
+    class ReportTool extends Tool implements Exportable {
+        
+        public void export () { // line n2
+            System.out.println("RTool::export");
+        }
+        
+        public static void main(String[] args) {
+            Tool aTool = new ReportTool();
+            Tool bTool = new Tool();
+            callExport(aTool);
+            callExport(bTool);
+        }
+        
+        public static void callExport (Exportable ex) {
+            ex.export();
+        }
+    }
+
