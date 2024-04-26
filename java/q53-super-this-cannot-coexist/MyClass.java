2f509d1d20e43145db3309cad23e309f0acb2670
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 12:17:38 -0400

    super and constructor
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q53-super-this-cannot-coexist> javac *.java
    MyClass.java:15: error: call to this must be first statement in constructor
            this(20); //line n2
                ^
    1 error

--- java/q53-super-this-cannot-coexist/MyClass.java
+++ java/q53-super-this-cannot-coexist/MyClass.java
@@ -1,12 +1,12 @@
 class Vehicle {
     int x;
     Vehicle() {
-    this(10); //line n1
+        this(10); //line n1
     }
     Vehicle(int x) {
-    this.x=x;
+        this.x=x;
     }
-p}
+}
 
 class Car extends Vehicle {
     int y;
