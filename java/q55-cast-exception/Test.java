eaef330aa1b16e6bcf1a8cc632471a138e0194b0
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 12:23:27 -0400

    incompatible type
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q55-cast-exception> javac *.java
    Test.java:9: error: incompatible types: possible lossy conversion from float to int
            iVar = fVar;
                   ^
    Test.java:11: error: incompatible types: possible lossy conversion from double to float
            fVar = dVar;
                   ^
    Test.java:15: error: incompatible types: possible lossy conversion from double to int
            iVar = dVar;
                   ^
    3 errors

---
+++ java/q55-cast-exception/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int iVar = 100;
+        float fVar = 100.100f;
+        double dVar = 123;
+
+        fVar = iVar;
+        iVar = fVar;
+
+        fVar = dVar;
+        dVar = fVar;
+
+        dVar = iVar;
+        iVar = dVar;
+    }
+}
