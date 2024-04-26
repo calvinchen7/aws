5f2d758ff69698860e2f4136bdbaaad87e7df7c1
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-26 09:21:49 -0400

    switch
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q21-switch> javac *.java
    Test.java:16: error: patterns in switch statements are a preview feature and are disabled by default.
                switch (bTest) {
                       ^
      (use --enable-preview to enable patterns in switch statements)
    Test.java:17: error: cannot find symbol
                    case ture:
                         ^
      symbol:   variable ture
      location: class Test
    2 errors

---
+++ java/q21-switch/Test.java
@@ -0,0 +1,26 @@
+public class Test {
+        public static void main(String[] args) {
+            String opt = "true";
+            
+            switch (opt) {
+                case "true":
+                System.out.print("True");
+                break;
+                default:
+                System.out.print("***");
+                
+            }
+            System.out.println("Done");
+        
+            boolean bTest = false;
+            switch (bTest) {
+                case ture:
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    
+    
+    }
