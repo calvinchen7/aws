36c6324911db67af90704bfc41e1aa7a6fe828bb
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-27 15:59:05 -0400

    constructor
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q171-constructor> javac *.java
    Test.java:32: error: no suitable constructor found for Employee(no arguments)
            Employee e1 = new Employee(); // Error
                          ^
        constructor Employee.Employee(String,int) is not applicable
          (actual and formal argument lists differ in length)
        constructor Employee.Employee(String,int,int) is not applicable
          (actual and formal argument lists differ in length)
    1 error

--- java/q162-interface-extends/Test.java
+++ java/q162-interface-extends/Test.java
@@ -21,8 +21,9 @@
         C2 obj1 = new C1();
         I obj2 = new C1();
         
-        //C2 s = (C2) obj2;
-        C2 s = obj2;
+        C2 s = (C2) obj2;
+
+        //C2 s = obj2;
         //Test.java:25: error: incompatible types: I cannot be converted to C2
         //C2 s = obj2;
         //       ^
