fb56b2386f57b75820c90e6c7970e0f5eecb48bd
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-28 10:35:01 -0400

    call the method within the class

--- java/q233-access-modifier/root/Tree.java
+++ java/q233-access-modifier/root/Tree.java
@@ -7,6 +7,9 @@ public class Tree {
 
     public void m1(){
         System.out.println("Tree m1");
+        this.m2();
+        this.m3();
+        this.m4();
     }
 
     private void m2(){
@@ -16,5 +19,7 @@ public class Tree {
         System.out.println("Tree m3");
     }
 
-    void m4(){}
+    void m4(){
+        System.out.println("Tree m4");
+    }
 }
