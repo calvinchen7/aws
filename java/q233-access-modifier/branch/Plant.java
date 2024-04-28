0b328e2b2ec24a04045d1f685944dca98c0c6402
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-28 10:28:16 -0400

    Intellij IDE - debug - public -private -protected -default

--- java/q233-access-modifier/branch/Plant.java
+++ java/q233-access-modifier/branch/Plant.java
@@ -3,9 +3,12 @@ package branch;
 import root.Tree;
 
 public class Plant extends Tree{
-    public void m1(){}
+    public Plant() {
+        System.out.println("Constructor Plant");
+    }
+    // public void m1(){}
     public void m2(){}
-    public void m3(){}
+    //public void m3(){}
     public void m4(){}
 
     public static void main(String[] args){
@@ -14,5 +17,8 @@ public class Plant extends Tree{
         t.m2();
         t.m3();
         t.m4();
+
+        Tree tree = new Plant();
+        tree.m1();
     }
 }
--- java/q233-access-modifier/root/Tree.java
+++ java/q233-access-modifier/root/Tree.java
@@ -1,7 +1,20 @@
 package root;
 public class Tree {
-    public void m1(){}
-    private void m2(){}
-    protected void m3(){}
+
+    public Tree() {
+        System.out.println("Constructor Tree");
+    }
+
+    public void m1(){
+        System.out.println("Tree m1");
+    }
+
+    private void m2(){
+        System.out.println("Tree m2");
+    }
+    protected void m3(){
+        System.out.println("Tree m3");
+    }
+
     void m4(){}
 }
