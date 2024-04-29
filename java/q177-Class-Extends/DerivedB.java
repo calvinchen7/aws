ad4cb044febbb35914582a532ca4b909c1cba125
Calvin Chen <Calvin.Chen@sita.aero>
2024-04-29 06:57:16 -0400

    cannot find symbol
    PS C:\Users\Calvin\Documents\GitHub\aws\java\q177-Class-Extends> javac *.java
    DerivedB.java:29: error: cannot find symbol
            b3.derivedBTest();
              ^
      symbol:   method derivedBTest()
      location: variable b3 of type Base
    1 error

--- java/q177-Class-Extends/DerivedB.java
+++ java/q177-Class-Extends/DerivedB.java
@@ -8,6 +8,12 @@ class DerivedB extends DerivedA {
     public void test() {
         System.out.println("DerivedB ");
     }
+
+    public void derivedBTest()
+    {
+        System.out.println("DerivedB Test");
+    }
+
     public static void main(String[] args) {
         Base b1 = new DerivedB();
         Base b2 = new DerivedA();
@@ -17,9 +23,19 @@ class DerivedB extends DerivedA {
         b1.test();
         b4.test();
 
-
         b1.baseTest();
         b4.baseTest();
-}
+
+        b3.derivedBTest();
+        /*
+        PS C:\Users\Calvin\Documents\GitHub\aws\java\q177-Class-Extends> javac *.java
+        DerivedB.java:29: error: cannot find symbol
+                b3.derivedBTest();
+                ^
+        symbol:   method derivedBTest()   
+        location: variable b3 of type Base
+        1 error
+        */
+    }
 
 }
