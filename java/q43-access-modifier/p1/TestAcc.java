30fd561d788012b8356ce1211caaa678b0a4e25e
Calvin Chen <Calvin.Chen@sita.aero>
2024-05-01 08:30:33 -0400

    access modifier - public -private -default -protected

---
+++ java/q43-access-modifier/p1/TestAcc.java
@@ -0,0 +1,13 @@
+
+package p1;
+
+public class TestAcc extends Acc {
+
+    public void TestAcc(){
+        TestAcc acc = new TestAcc();
+        System.out.println(acc.p);
+        System.out.println(acc.q);
+        System.out.println(acc.r);
+        System.out.println(acc.s);
+    }
+}
--- java/q43-access-modifier/p2/Test.java
+++ java/q43-access-modifier/p2/Test.java
@@ -3,16 +3,22 @@
 package p2;
 import p1.Acc;
 
-public class Test extends Acc {
+public class TestAcc {
 
     public static void main(String[] args) {
 
-        Acc obj = new Test();
+        Acc obj1 = new Acc();
+        System.out.println(obj1.p);
+        System.out.println(obj1.q);
+        System.out.println(obj1.r);
+        System.out.println(obj1.s);
 
+        Acc obj = new Test();
         System.out.println(obj.p);
         System.out.println(obj.q);
         System.out.println(obj.r);
         System.out.println(obj.s);
+
 
     }
 }
